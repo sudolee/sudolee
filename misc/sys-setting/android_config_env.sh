55b635835a5634a16b30c6effc82139d16023b08
sudolee <desoxydate@gmail.com>
2013-04-12 14:53:57 +0800

    update scripte

--- misc/sys-setting/android_config_env.sh
+++ misc/sys-setting/android_config_env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # http://source.android.com/source/initializing.html
 
@@ -15,15 +15,13 @@ if [ 32 = "$BIT" ];then
 	  libxml2-utils xsltproc zlib1g-dev
 elif [ 64 = "$BIT" ];then
 	ARCH=iamd64
-	sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl \
-		zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs x11proto-core-dev libx11-dev \
-		lib32readline5-dev lib32z-dev libgl1-mesa-dev g++-multilib mingw32 tofrodos \
+	sudo apt-get install gnupg flex bison gperf build-essential curl \
+		zlib1g-dev libc6-dev lib32ncurses6-dev ia32-libs x11proto-core-dev libx11-dev \
+		lib32readline6 lib32z1-dev libgl1-mesa-dev g++-multilib mingw32 tofrodos \
 		python-markdown libxml2-utils xsltproc
 else
 	echo "Sorry, neither 32-bits nor 64-bits, no software matched."
 fi
-
-./apt_get.sh
 
 curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > ./repo
 chmod +x repo
--- misc/sys-setting/apt_get.sh
+++ misc/sys-setting/apt_get.sh
@@ -3,15 +3,14 @@
 # Just install useful softwares...
 #
 
-sudo apt-get update
-sudo apt-get install gcc g++ make gdb bison libc6 libc6-dev manpages manpages-posix manpages-dev manpages-posix-dev glibc-doc \
-	subversion git-core gitk libncurses5 libncurses5-dev im-switch tree indent patch tar rar unzip unrar vim \
-	putty tftp vsftpd wget smbfs cramfsprogs cscope ctags meld sharutils ssh perl bash python \
-	soundconverter gawk sed filezilla -y
+sudo apt-get install gcc g++ gcc-multilib g++-multilib make gdb bison libc6 libc6-dev manpages manpages-posix manpages-dev manpages-posix-dev glibc-doc \
+	subversion git-core gitk libncurses5 libncurses5-dev im-switch tree indent patch tar zip rar unzip unrar vim \
+	putty axel wget cramfsprogs cscope ctags diffutils meld sharutils ssh perl bash python \
+	soundconverter gawk sed build-essential -y
 
 
-# filezilla vim-gnome iptux kchmviewer gdebi minicom
-# bluefish sqlite3 sqlite3-doc libsqlite3-0 libsqlite3-dev lftp atftp atftpd nfs-kernel-server openssh-client openssh-server gimp thunderbird
+# filezilla vim-gnome iptux kchmviewer gdebi minicom tftp vsftpd
+# bluefish sqlite3
 # gparted transmission-cli shotwell
 # lingot ## for musical tuner.
 
