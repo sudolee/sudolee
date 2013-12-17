12d12aadd496fa35b4bb7714c1b17284f2fd4302
matti <desoxydate@gmail.com>
2013-12-17 20:22:23 +0800

    archlinux : add utc time in windows7

--- misc/git_setting.sh
+++ misc/git_setting.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-
 git config --global core.editor 'vim'
 git config --global user.name 'matti'
 git config --global user.email 'desoxydate@gmail.com'
 
 git config --global color.branch always
 git config --global color.status always
-
-return
