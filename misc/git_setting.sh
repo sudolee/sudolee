5e7ef9fb1e8a101b08b401abbb5af5d2894443ed
mti <sudolee@163.com>
2014-02-28 21:22:36 +0800

    arch_install.sh update : fixed thinkpad touchpad setting.

--- misc/git_setting.sh
+++ misc/git_setting.sh
@@ -1,7 +1,6 @@
 git config --global core.editor 'vim'
-git config --global push.default simple
-git config --global user.name 'matti'
-git config --global user.email 'desoxydate@gmail.com'
+git config --global user.name 'mti'
+git config --global user.email 'sudolee@163.com'
 
 git config --global color.branch always
 git config --global color.status always
