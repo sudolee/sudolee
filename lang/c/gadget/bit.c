4018a808cb9139908b5368e522143e6452d83837
mti <sudolee@163.com>
2014-03-07 22:39:05 +0800

    bit.c update: base update to 32-bits.

--- lang/c/gadget/bit.c
+++ lang/c/gadget/bit.c
@@ -1,3 +1,9 @@
+/*
+ * Note:
+ *  Bit only set type as unsigned short [0~65535],
+ *  because it's enough for udp port.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,25 +13,25 @@
  * @ arr: bitmap
  * @ bit: the bit to set
  */
-static inline unsigned char check_bit(unsigned char *arr, unsigned short bit)
+static inline unsigned int check_bit(unsigned int *arr, unsigned short bit)
 {
-	return (*(arr + (bit >> 3)) & (0x1 << (bit % 8))) ? 1 : 0;
+	return (*(arr + (bit >> 5)) & (1 << (bit % 32))) ? 1 : 0;
 }
 
 /*
  * Set a bit in bitmap
  */
-static inline void set_bit(unsigned char *arr, unsigned short bit)
+static inline void set_bit(unsigned int *arr, unsigned short bit)
 {
-	*(arr + (bit >> 3)) |= 0x1 << (bit % 8);
+	*(arr + (bit >> 5)) |= 1 << (bit % 32);
 }
 
 /*
  * Clear a bit in bitmap
  */
-static inline void clear_bit(unsigned char *arr, unsigned short bit)
+static inline void clear_bit(unsigned int *arr, unsigned short bit)
 {
-	*(arr + (bit >> 3)) &= ~(0x1 << (bit % 8));
+	*(arr + (bit >> 5)) &= ~(1 << (bit % 32));
 }
 
 /*
@@ -34,7 +40,8 @@ static inline void clear_bit(unsigned char *arr, unsigned short bit)
  * @ Start : the bit to start counting from
  * @ End   : the bit to end   counting
  */
-static inline void set_bit_range(unsigned char *arr, unsigned short Start, unsigned short End)
+static inline void set_bit_range(unsigned int *arr,
+		unsigned short Start, unsigned short End)
 {
 	do {
 		set_bit(arr, Start++);
@@ -44,14 +51,15 @@ static inline void set_bit_range(unsigned char *arr, unsigned short Start, unsig
 /*
  * Clear range of bit in bitmap
  */
-static inline void clear_bit_range(unsigned char *arr, unsigned short Start, unsigned short End)
+static inline void clear_bit_range(unsigned int *arr,
+		unsigned short Start, unsigned short End)
 {
 	do {
 		clear_bit(arr, Start++);
 	} while (Start <= End);
 }
 
-static void bits_dump(unsigned char *arr, size_t size)
+static void bits_dump(unsigned int *arr, size_t size)
 {
 	int i;
 
@@ -96,24 +104,33 @@ int get_range(unsigned char *src, unsigned short *range)
 
 int main(int argc, char **argv)
 {
+	unsigned int arr[8] = {0};
+	unsigned int max_bit = (sizeof(arr) << 3) - 1;
+
 	if(argc < 2) {
-		printf("Pls enter bit number {0-255} to set/clear/check.\n");
+		printf("Pls enter bit number {0-%u} to set/clear/check.\n", max_bit);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("Bit [%s] will be set and clear.\n", argv[1]);
 	}
 
-	unsigned char  arr[8] = {0};
 	unsigned short range[2] = {0};
 
 	if(-1 == get_range(argv[1], range)) {
 		printf("Oops, invalid range, :(\n");
 		exit(EXIT_FAILURE);
+	} else {
+		if((range[0] > max_bit) || (range[1] > max_bit)) {
+			printf("Oops, range out of {0-%u}, :(\n", max_bit);
+			exit(EXIT_FAILURE);
+		}
 	}
 
+	printf("Set bit(s)...\n");
 	set_bit_range(arr, range[0], range[1]);
 	bits_dump(arr, sizeof(arr));
 
+	printf("Then, clear set bit(s)...\n");
 	clear_bit_range(arr, range[0], range[1]);
 	bits_dump(arr, sizeof(arr));
 
