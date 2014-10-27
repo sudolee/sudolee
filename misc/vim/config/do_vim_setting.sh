a801a86b1647d08b77500f92ecc9520ad6912ec5
Matti Lee <desoxydate@gmail.com>
2014-10-27 12:16:32 +0800

    vim plugins update: + snipMate + snippets + clang_complete + tlib + mw-utils - NERD_tree

--- misc/vim/config/do_vim_setting.sh
+++ misc/vim/config/do_vim_setting.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash -e
 
 rm -fr ~/.vim
 rm -f ~/.vimrc
 cp -fr vim ~/.vim
 cp -f vimrc ~/.vimrc
 
-
 echo \
-'------------------------------------------------------------
-Installed plugins for vim:
-	+ taglist.vim
-	+ cscope_macros.vim
-	+ NERD_tree.vim
-	Any issure, pls see ~/.vim/doc/ for detials
-------------------------------------------------------------'
+'::Installed plugins for vim:
+    + cscope_macros.vim
+    + taglist.vim
+    + snipMate
+    + snippets
+    + clang_complete
+    + tlib
+    + mw-utils
+:: Any issure, pls see ~/.vim/doc/ for detials.'
