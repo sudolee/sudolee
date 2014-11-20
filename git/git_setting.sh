e78196799d53bf84b415ca9849bb1878b4b773e5
Matti Lee <desoxydate@gmail.com>
2014-11-20 20:39:07 +0800

    goagent-start script added

--- git/git_setting.sh
+++ git/git_setting.sh
@@ -1,3 +1,3 @@
 git config --global core.editor 'vim'
-git config --global user.name 'Matti Lee'
-git config --global user.email 'desoxydate@gmail.com'
+git config --global user.name   'Matti Lee'
+git config --global user.email  'desoxydate@gmail.com'
