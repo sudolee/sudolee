c54ed83ff1e6b236f40d6363f780cec2c40ac911
Matti Lee <lipanfeng@teraspek.com>
2017-07-31 09:48:41 +0800

    git config add http settings

--- git/git_setting.sh
+++ git/git_setting.sh
@@ -1,4 +1,6 @@
 git config --global core.editor 'vim'
 git config --global user.name   'Matti Lee'
 git config --global user.email  'desoxydate@gmail.com'
-git config --global core.autocrlf false
+#git config --global core.autocrlf false
+git config --global http.sslVerify false
+git config --global http.postBuffer 524288000
